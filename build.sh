bcf2f2637f4ea69aebdd593fc670a861895bbfa4
Everton Paiva <evertonpaiva@gmail.com>
2020-06-10 11:42:15 -0300

    Issue #8. Atualizando imagem antes do build.

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@
 
 set -e
 
+echo -e "\nAtualizando imagem base"
+docker pull hub.dds.ufvjm.edu.br/desenvolvimento/alpine-php7:latest
+
 echo -e "\nConstruindo a imagem localmente"
 docker-compose build
