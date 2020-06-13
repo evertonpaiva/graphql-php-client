469bf44cb8a632d83a6a7c4ddd753a27b01e7d6c
Everton Paiva <evertonpaiva@gmail.com>
2020-06-13 12:19:27 -0300

    Issue #9. Criando classe para tipo de instância de paginação incorreta.

---
+++ src/Exception/WrongInstancePaginationException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace GraphqlClient\Exception;
+
+class WrongInstancePaginationException extends \Exception
+{
+    public function __construct($className, $relationName = null)
+    {
+        $message = 'Erro ao criar paginação para classe '.$className;
+
+        if(!is_null($relationName)){
+            $message .= 'Paginação do relacionamento '.$relationName.' não é da classe de paginações.';
+        }
+        parent::__construct($message);
+    }
+}
