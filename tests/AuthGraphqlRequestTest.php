88e32faf336149ce08ff89535d4bca0fd7e761bb
Everton Paiva <everton.paiva@ufvjm.edu.br>
2022-08-29 08:23:16 -0300

    Implementando query para recuperar informações do servidor. Corrigindo formato dos tokens de teste.

--- tests/AuthGraphqlRequestTest.php
+++ tests/AuthGraphqlRequestTest.php
@@ -40,14 +40,30 @@ class AuthGraphqlRequestTest extends GraphqlRequestTest
         $this->expectException(DecodeTokenException::class);
 
         Session::startSession();
-        Session::put(ConfigRequest::SESSION_APP_HEADER_NAME, 'Bearer xxx');
-        Session::put(ConfigRequest::SESSION_USER_HEADER_NAME, 'Bearer yyy');
+        Session::put(ConfigRequest::SESSION_APP_HEADER_NAME, 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImFwcC10b2tlbi1rZXkifQ.eyJzdWIiOiJjNGRkZDQyOC01MzQwLTQ0MTUtYTZkYi04YzU5YTUyOWQ5MDMiLCJpZEFwbGljYXRpdm8iOjUsIm5vbWUiOiJTSUVYQyAtIEJvbHNhcyIsImFwcElkIjoiYzRkZGQ0MjgtNTM0MC00NDE1LWE2ZGItOGM1OWE1MjlkOTAzIiwiaWF0IjoxNjYxNTQxMDUwLCJleHAiOjE2NjE2Mjc0NTAsImlzcyI6Imh0dHA6Ly8xMC4yNTQuMTYuMTQzOjMwMDk5In0.fSUtUsE8KpJ8GQybBq5Rh8FqZ_DrZSvhtE9M_t9ufGkV95AeAPy_zQuP4jNE2TkfCxyjF7jMwTM1PwodKquha0vlLcYiG-bSJ9fC5ktKhCzTEIUndyNUDTr8gj4G0H88OLg2eK_XYHb0M75pflSzJDG20RsuGrV-0C8OK8e-BSIdYULDw233Bg2an8jD87f5FWzTA9DKp8AVicBQNZ6rehHWHuP3o64TnI7bC0xGWcREHM4avr3PMnaC7YOdw70xsYXTN2j9a8dE4ljZzoh9R0a7BUHeCduRLfjZ2v7YE7mDiCY88RMDyJszJIc2yzQJOlxLjJ-DYqtT2Vtl7dszeIRPW9FViCmp9iSp4njWcad4eglaIiIwA7yzrQNXm9XyvNnzY_7sCYUk7HEEAknpuxogHeoN6ebbI1bcF1ER0ClroUjcnOSVaeEX0y6VCdQNeT22p8wtDtS4uzA_BLGtsduNAoGOQenvOBAC7j_yZv-C8XxvQMJ3h5s44zSj09AFbzUXlMLKgg_gy9373WmxFclsRTZRCeOV2Jln5Q6vXJgIiXYyGOhiowPX9ttuoiD6mtD_qF7JLd2k4r1I78k7CgTHjPmasWFLpv5sjLd8-7-rLcjQAbpZOUmnu-Y_0z9e64cL5s27Xh8NEPlwkPL0vENDdPBVK1kt09OpZLiQ7Iy');
+        Session::put(ConfigRequest::SESSION_USER_HEADER_NAME, 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImFwcC10b2tlbi1rZXkifQ.eyJzdWIiOiJjNGRkZDQyOC01MzQwLTQ0MTUtYTZkYi04YzU5YTUyOWQ5MDMiLCJpZEFwbGljYXRpdm8iOjUsIm5vbWUiOiJTSUVYQyAtIEJvbHNhcyIsImFwcElkIjoiYzRkZGQ0MjgtNTM0MC00NDE1LWE2ZGItOGM1OWE1MjlkOTAzIiwiaWF0IjoxNjYxNTQxMDUwLCJleHAiOjE2NjE2Mjc0NTAsImlzcyI6Imh0dHA6Ly8xMC4yNTQuMTYuMTQzOjMwMDk5In0.fSUtUsE8KpJ8GQybBq5Rh8FqZ_DrZSvhtE9M_t9ufGkV95AeAPy_zQuP4jNE2TkfCxyjF7jMwTM1PwodKquha0vlLcYiG-bSJ9fC5ktKhCzTEIUndyNUDTr8gj4G0H88OLg2eK_XYHb0M75pflSzJDG20RsuGrV-0C8OK8e-BSIdYULDw233Bg2an8jD87f5FWzTA9DKp8AVicBQNZ6rehHWHuP3o64TnI7bC0xGWcREHM4avr3PMnaC7YOdw70xsYXTN2j9a8dE4ljZzoh9R0a7BUHeCduRLfjZ2v7YE7mDiCY88RMDyJszJIc2yzQJOlxLjJ-DYqtT2Vtl7dszeIRPW9FViCmp9iSp4njWcad4eglaIiIwA7yzrQNXm9XyvNnzY_7sCYUk7HEEAknpuxogHeoN6ebbI1bcF1ER0ClroUjcnOSVaeEX0y6VCdQNeT22p8wtDtS4uzA_BLGtsduNAoGOQenvOBAC7j_yZv-C8XxvQMJ3h5s44zSj09AFbzUXlMLKgg_gy9373WmxFclsRTZRCeOV2Jln5Q6vXJgIiXYyGOhiowPX9ttuoiD6mtD_qF7JLd2k4r1I78k7CgTHjPmasWFLpv5sjLd8-7-rLcjQAbpZOUmnu-Y_0z9e64cL5s27Xh8NEPlwkPL0vENDdPBVK1kt09OpZLiQ7Ix');
 
         // Carrega a classe de autenticação
         $authGraphqlRequest = new AuthGraphqlRequest();
 
         // Recupera as informações do usuário logado
         $authGraphqlRequest->usuarioLogadoInfo();
+        Session::forget(ConfigRequest::SESSION_APP_HEADER_NAME);
+        Session::forget(ConfigRequest::SESSION_USER_HEADER_NAME);
+    }
+
+    /**
+     * Testa informações do servidor.
+     */
+    public function testServerInfo()
+    {
+        // Carrega a classe de autenticação
+        $authGraphqlRequest = new AuthGraphqlRequest();
+
+        // Tenta realizar o login na Conta Institucional
+        $serverInfo = $authGraphqlRequest->serverInfo();
+        $this->assertNotNull($serverInfo);
+        $this->assertEquals($serverInfo->name, 'Microsserviços');
     }
 
     /**
